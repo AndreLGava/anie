dd61dfcbfcba87554a6d7451f58dd8e633fe48d8
Gabriel Masson <gabrielmasson.a3912@gmail.com>
2016-07-10 11:51:33 -0300

    0.9

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# anie
-Módulo CSS para Bootstrap
+# Anie 0.9
+CSS module for Bootstrap
+
+## Documentation
+Check out how to use the Anie classes in the examples of ***/examples***
---
+++ _assets/js/anie.js
@@ -0,0 +1,9 @@
+/*
+  Anie 1.0
+  https://github.com/dinfo21/anie
+  License MIT
+*/
+
+function print() { window.print(); }
+function block(id) { document.getElementById(id).style.display="block"; }
+function none(id) { document.getElementById(id).style.display="none"; }
