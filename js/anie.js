c6361b261d77b899b2935e2ecacabfb0322c06dc
Gabriel Masson <gabrielmasson.a3912@gmail.com>
2017-01-21 04:11:37 -0200

    1.1.5

--- js/anie.js
+++ js/anie.js
@@ -1,5 +1,5 @@
 /*
-  Anie 1.1.0
+  Anie 1.1.5
   https://github.com/gmasson/anie
   License MIT
 */
@@ -10,3 +10,35 @@ function print() { window.print() }
 function back() { window.history.go(-1) }
 function next() { window.history.go(1) }
 function reload() { window.history.go(0) }
+
+function addtext(idElement, text) {
+  var area = document.getElementById(idElement);
+  var cursorPosition = area.selectionStart;
+  cursorposition(area, cursorPosition, text);
+  updatecursorposition(cursorPosition, text, area);
+}
+
+function cursorposition(area, cursorPosition, text) {
+  var front = (area.value).substring(0, cursorPosition);
+  var back = (area.value).substring(cursorPosition, area.value.length);
+  area.value = front + text + back;
+}
+
+function updatecursorposition(cursorPosition, text, area) {
+  cursorPosition = cursorPosition + text.length;
+  area.selectionStart = cursorPosition;
+  area.selectionEnd = cursorPosition;
+  area.focus();    
+}
+
+function savetext(idElement, nameElement) {
+  var textscreen = document.getElementById(idElement).value;
+  window.localStorage.setItem(nameElement, textscreen);
+}
+
+function recovertext(idElement, nameElement) {
+  var text = window.localStorage.getItem(nameElement);
+  if (text == null) { var text = ''; }
+  addtext(idElement, text);
+  text.download = "teste.html";
+}
